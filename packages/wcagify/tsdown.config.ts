4fc7cc73009614692b108c4880bb74d288731b73
timdamen <tim.damen80@gmail.com>
2026-03-07 21:33:05 +0100

    chore: revert nuxi ts linking

--- packages/wcagify/tsdown.config.ts
+++ packages/wcagify/tsdown.config.ts
@@ -3,8 +3,7 @@ import { defineConfig } from 'tsdown'
 export default defineConfig({
   entry: ['src/index.ts', 'src/pdf/index.ts', 'src/module.ts', 'src/config.ts', 'src/content.ts'],
   format: 'esm',
-  tsconfig: 'tsconfig.build.json',
-  dts: { tsconfig: 'tsconfig.build.json' },
+  dts: true,
   outDir: 'dist',
   clean: true,
   hash: false,
