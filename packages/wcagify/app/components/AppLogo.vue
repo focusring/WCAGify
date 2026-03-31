883854fceb4f24915f5a72603745365be4e27a34
timdamen <hello@timdamen.io>
2026-03-31 20:53:35 +0200

    fix add home label to logo link

--- packages/wcagify/app/components/AppLogo.vue
+++ packages/wcagify/app/components/AppLogo.vue
@@ -1,3 +1,16 @@
 <template>
   <UIcon name="i-logo:wcagify" class="shrink-0 size-28 text-black dark:text-white" />
+  <span class="visually-hidden">Home</span>
 </template>
+
+<style scoped>
+.visually-hidden:not(:focus):not(:active) {
+  clip: rect(0 0 0 0);
+  clip-path: inset(50%);
+  height: 1px;
+  overflow: hidden;
+  position: absolute;
+  white-space: nowrap;
+  width: 1px;
+}
+</style>
