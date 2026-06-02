e11351a453a48c4a3d7a3f1f06a3c6aefd541a66
Bryan O.F. <brian.ortizfranco@hotmail.com>
2026-06-02 22:52:48 +0200

    fix(wcagify): fix badge variant and test PDF report fix
    Fixed badge warning using the wrong variant. Applied a test fix for the PDF version of the report, still needs further testing.

--- packages/wcagify/app/components/ReportSuccessCriterion.vue
+++ packages/wcagify/app/components/ReportSuccessCriterion.vue
@@ -18,44 +18,63 @@ const statusFilters = inject<Ref<Set<string>>>('statusFilters')
     class="rounded-lg border border-muted bg-muted overflow-hidden"
   >
     <div class="flex items-center gap-3 px-4 py-3">
-      <UBadge :label="criterion.level" variant="subtle" class="shrink-0" />
+      <!-- <UBadge :label="criterion.level" variant="subtle" class="shrink-0 print:hidden" /> -->
+
+      <div class="bd bd-primary hidden print:block">
+        <span>{{ criterion.level }}</span>
+      </div>
 
       <h4 class="font-medium text-highlighted text-base w-full">
         {{ criterion.name }}
       </h4>
 
-      <UBadge
-        v-if="criterion.status === 'passed'"
-        :label="t('report.scStatus.passed')"
-        color="success"
-        variant="subtle"
-        icon="i-lucide-check"
-        class="shrink-0"
-      />
-      <UBadge
-        v-else-if="criterion.status === 'not-present'"
-        :label="t('report.scStatus.not-present')"
-        color="info"
-        variant="subtle"
-        icon="i-lucide-book-dashed"
-        class="shrink-0"
-      />
-      <UBadge
-        v-else-if="criterion.status === 'not-tested'"
-        :label="t('report.scStatus.not-tested')"
-        color="error"
-        variant="subtle"
-        icon="i-lucide-mouse-pointer-2-off"
-        class="shrink-0"
-      />
-      <UBadge
-        v-else-if="criterion.status === 'failed'"
-        :label="t('report.scStatus.failed')"
-        color="error"
-        variant="subtle"
-        icon="i-lucide-x"
-        class="shrink-0"
-      />
+      <!-- Screen reader Badge only -->
+      <div class="shrink-0 print:hidden">
+        <UBadge
+          v-if="criterion.status === 'passed'"
+          :label="t('report.scStatus.passed')"
+          color="success"
+          variant="subtle"
+          icon="i-lucide-check"
+        />
+        <UBadge
+          v-else-if="criterion.status === 'not-present'"
+          :label="t('report.scStatus.not-present')"
+          color="info"
+          variant="subtle"
+          icon="i-lucide-book-dashed"
+        />
+        <UBadge
+          v-else-if="criterion.status === 'not-tested'"
+          :label="t('report.scStatus.not-tested')"
+          color="warning"
+          variant="subtle"
+          icon="i-lucide-mouse-pointer-2-off"
+        />
+        <UBadge
+          v-else-if="criterion.status === 'failed'"
+          :label="t('report.scStatus.failed')"
+          color="error"
+          variant="subtle"
+          icon="i-lucide-x"
+        />
+      </div>
+
+      <!-- Print-only Badges -->
+      <div class="shrink-0 hidden print:block">
+        <div class="bd bd-success" v-if="criterion.status === 'passed'">
+          <span>{{ t('report.scStatus.passed') }}</span>
+        </div>
+        <div class="bd bd-info" v-else-if="criterion.status === 'not-present'">
+          <span>{{ t('report.scStatus.not-present') }}</span>
+        </div>
+        <div class="bd bd-warning" v-else-if="criterion.status === 'not-tested'">
+          <span>{{ t('report.scStatus.not-tested') }}</span>
+        </div>
+        <div class="bd bd-error" v-else-if="criterion.status === 'failed'">
+          <span>{{ t('report.scStatus.failed') }}</span>
+        </div>
+      </div>
     </div>
 
     <div v-if="criterion.issues.length > 0">
